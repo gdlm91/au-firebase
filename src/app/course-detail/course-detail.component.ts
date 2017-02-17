caf340ee47578c2b843df19dd342f1b8f786e98a
Gerardo Leal <gerard.leal91@gmail.com>
2017-02-17 18:39:05 -0300

    Course Details

--- src/app/course-detail/course-detail.component.ts
+++ src/app/course-detail/course-detail.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs';
+import { Lesson } from './../shared/model/lesson';
+import { Course } from './../shared/model/course';
+import { CoursesService } from './../shared/model/courses.service';
 
 @Component({
   selector: 'app-course-detail',
@@ -7,9 +12,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CourseDetailComponent implements OnInit {
 
-  constructor() { }
+  course$: Observable<Course>;
+  lessons$: Observable<Lesson[]>;
+
+  constructor(
+    private route: ActivatedRoute,
+    private courseService: CoursesService
+  ) { }
 
   ngOnInit() {
+    const courseUrl = this.route.snapshot.params['id'];
+
+    this.course$ = this.courseService.findCourseByUrl(courseUrl);
+
+    this.lessons$ = this.courseService.findLessonsForCourse(courseUrl);
   }
 
 }
--- src/app/shared/model/courses.service.ts
+++ src/app/shared/model/courses.service.ts
@@ -1,17 +1,40 @@
 import { Injectable } from '@angular/core';
-import { AngularFire } from 'angularfire2';
+import { AngularFireDatabase } from 'angularfire2';
 import { Observable } from 'rxjs';
 import { Course } from './course';
+import { Lesson } from './lesson';
 
 @Injectable()
 export class CoursesService {
 
   constructor(
-    private af: AngularFire
+    private afDB: AngularFireDatabase
   ) { }
 
   findAllCourses(): Observable<Course[]> {
-    return this.af.database.list('courses').map(Course.fromJsonList);
+    return this.afDB.list('courses').map(Course.fromJsonList);
+  }
+
+  findCourseByUrl(courseUrl: string): Observable<Course> {
+    return this.afDB.list('courses', {
+      query: {
+        orderByChild: 'url',
+        equalTo: courseUrl
+      }
+    })
+      .map(results => results[0]);
+  }
+
+  findLessonKeysPerCourseUrl(courseUrl: string): Observable<string[]> {
+    return this.findCourseByUrl(courseUrl)
+      .switchMap(course => this.afDB.list('lessonsPerCourse/' + course.$key))
+      .map(lspc => lspc.map(lpc => lpc.$key));
+  }
+
+  findLessonsForCourse(courseUrl: string): Observable<Lesson[]> {
+    return this.findLessonKeysPerCourseUrl(courseUrl)
+      .map(lspc => lspc.map(lessonKey => this.afDB.object('lessons/' + lessonKey)))
+      .flatMap(fbobjs => Observable.combineLatest(fbobjs));
   }
 
 }
